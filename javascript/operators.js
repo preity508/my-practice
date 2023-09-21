6c875c89fb7941d0f30a5c4ea51d44e907394bae
preity508 <preitypatil2022@gmail.com>
2023-09-21 08:28:23 +0530

    opratorjs

---
+++ javascript/operators.js
@@ -0,0 +1,70 @@
+let num =20 + 50
+console.log('number variable value', num);
+
+
+let a = 20
+console.log(a++);
+
+
+let v = 70;
+v += 50;
+console.log(v);
+
+let b = 30;
+b -= 5;
+console.log(b);
+
+
+console.log('greater than', v > b)
+
+let number = 80 - 30;
+console.log(number);
+
+let n = 40;
+console.log(++n);
+
+let f = 40;
+f += 40;
+console.log(f);
+
+console.log( f < n);
+console.log(f == n);
+console.log( f == n);
+let u = 50;
+let w = '50';
+console.log('check value', u == w);
+console.log('check value and data' , u === w);
+
+
+console.log( u > w);
+console.log( u < w);
+
+
+let r = 40;
+let s = 30;
+console.log( r > s);
+console.log( r < s);
+
+console.log('--> not equal/value are not same', r != s);
+console.log('--> not equal value or not equal data', r !== s);
+
+console.log('>=', r >= s);
+console.log('<=', s >= r);
+console.log('<=', r <= s);
+
+let d = 700;
+d > 600 ? console.log('d is greater') : console.log('d is smaller');
+
+let text1 = 'A';
+let text2 = 'A' ;
+let result = text1 === text2;
+console.log(result);
+
+let r1 = 90;
+let r2 = 70;
+console.log('&&-->', r1 > r2 && r2 < r1);
+
+console.log('||-->', r1 < r2 || r2 < r1);
+
+let g = false;
+console.log('!-->',  !g);
