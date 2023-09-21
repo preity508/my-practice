836e0bf2e86b36f29f7c5164a07505a65bf9b733
preity508 <preitypatil2022@gmail.com>
2023-09-21 14:29:48 +0530

    opratorjs

--- javascript/operators.js
+++ javascript/operators.js
@@ -68,3 +68,66 @@ console.log('||-->', r1 < r2 || r2 < r1);
 
 let g = false;
 console.log('!-->',  !g);
+ 
+
+
+
+let z = 30;
+z += 30;
+console.log(z);
+
+let c = 20;
+let h = 40;
+console.log( c < d);
+
+let x = 40;
+let y = 35;
+console.log(x == y);
+console.log( x === y);
+console.log( x != y);
+console.log( x !== y );
+console.log( x <= y);
+
+
+let a1 = 10;
+let a2 = '10';
+console.log('a1,a2', a1 == a2);
+console.log( a1 === a2);
+
+// greter than //.....<  >......
+console.log( a1 > a2);
+
+//!=  !== //not value equal to and not value or data equal to
+
+console.log('!=', a1 != a2);
+console.log('!==', a1 !== a2);
+
+let b1 = 40;
+let b2 = 30;
+console.log(b1 >= b2 );
+console.log( b1 <= b2);
+
+//? : ternory oprater ::: codition ?true : false//
+
+
+let c1 = 300;
+c1 > 200 ? console.log( 'c1 is greater')  : console.log( 'c1 is smaller');
+
+
+let v1 = 500;
+v1 > 300 ? console.log('v1 is greater') : console.log(' v1 is smaller');
+
+let text3 = 'B'
+let text4 = 'B'
+let rsl = text3 == text4
+console.log(rsl);
+
+let r3 = 60;
+let r4 =50;
+
+console.log( '&&', r3 > r4 && r4 < r3  );// &&
+console.log('||', r3 < r4 || r4 < r3); // || //
+
+
+let z1 = false; //...!...//
+console.log( '!',!z1);
