9c69a4f8005f6508ab918d9571821a0ab316681b
preity508 <preitypatil2022@gmail.com>
2023-09-29 12:26:49 +0530

    loops.js

--- javascript/conditions.js
+++ javascript/conditions.js
@@ -10,23 +10,23 @@ if (a > b) {
 }
 
 if (a < b) {
-   console.log( 'a is small'); 
+    console.log('a is small');
 }
 else {
     console.log('a is big');
-//else if ->> if want to check more than 2 conditions
+    //else if ->> if want to check more than 2 conditions
 }
-if ( a == b) {
+if (a == b) {
     console.log(' a and b are equal');
-    
+
 }
 else if (a < b) {
-    console.log( 'a is smaller than b');
-    
+    console.log('a is smaller than b');
+
 }
 else if (b > a) {
     console.log(' b is greater than a');
-    
+
 }
 
 
@@ -39,11 +39,74 @@ switch (color) {
     case 'black':
         console.log('color is black');
         break;
-        case 'white' :
-            console.log('color is white');
-            break;
+    case 'white':
+        console.log('color is white');
+        break;
 
     default:
         console.log('no color');
         break;
+}
+let a1 = 80;
+let b1 = 60;
+
+if (a1 > b1) {
+    console.log('a1 is big than b1 ');
+
+}
+
+if (b1 < a1) {
+    console.log('b1 is smaller than a1');
+
+}
+else {
+    console.log('a1 is big');
+}
+///-------------------///
+if (a1 == b1) {
+
+    console.log('a1 and b1 are equal');
+
+}
+else if (a1 > b1) {
+    'a1 is greater than b1'
+
+}
+else if (a1 < b1) {
+    console.log('a1 is less than b1');
+
+}
+else {
+    console.log('else block');
+}
+let clr = 'orange'
+switch (clr) {
+    case 'black':
+        console.log('clr is black');
+       break;
+
+
+    case' white' :
+        console.log('clr is orange');
+         break;
+
+
+    case' pink ':
+        console.log(' clr is pink');
+    break;
+
+    //default:
+       // console.log('no match');
+       // break;
+}
+
+let x = 30;
+let y = 150;
+
+if( x < 100){
+      if(x < y){
+        if(y < 100){
+            console.log(y);
+        }
+      }
 }
---
+++ javascript/loops.js
@@ -0,0 +1,51 @@
+
+//for loop 
+// array
+
+
+let cars = ['i20', 'i10', 'swift', 'jaguar', 'audi']
+console.log(cars.length);
+
+console.log(cars[0]);
+console.log(cars[1]);
+console.log(cars[2]);
+console.log(cars[3]);
+console.log(cars[4]);
+
+//for (let i = 0; i < cars.length; i++) {
+   // console.log(cars[i]);
+//
+
+
+ //   console.log(cars[i]);
+
+
+//for (let in cars) {console.log(cars [i]);}
+
+
+
+let student = {
+    name: 'nitin',
+    age:20,
+    mob: 6574839201
+    
+}
+for (let x in student){
+console.log(student[x]);
+}
+
+
+let numbers =[10,30,40,50,60,889]
+
+for (let num of numbers) {
+    console.log(num);
+    
+}
+    let h = 10;
+    
+    do {
+        h++
+        console.log(h);
+    } while (h  < 15);
+
+
