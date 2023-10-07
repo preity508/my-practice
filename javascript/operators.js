79d874d70f7480480cf871ff763a4990e1b21e9c
preity508 <preitypatil2022@gmail.com>
2023-10-07 14:24:06 +0530

    operator.js

--- javascript/operators.js
+++ javascript/operators.js
@@ -20,8 +20,77 @@ console.log(b + 10);
 
 let = c = 40;
 c + 30;;
-console.log(c - 30);
+console.log(c + 30);
 let r = 50;
 let v = 30;
 v += r;
 console.log('v', v);
+//comparison operators -->> true/ false// ==  : equality operator 
+let f = 80;
+let g = 80;
+console.log( f==g);
+
+
+let o = 90;
+let p = 70;
+console.log(o===p);
+
+console.log('>',  o < p);
+console.log ('>', f < g);
+
+console.log('!=', o != p);
+console.log('!=', f !=g);
+console.log('!==', o !==p);
+console.log('!==', f !== g);
+
+console.log('>=', o <= p);
+console.log(' f g <=', f >= g);
+
+let k = 10;
+k > 50 ?  console.log('k is greater')  :  console.log('k is smaller');
+
+let a1 = 60;
+a1 > 50 ? console.log('a1 is greater') : console.log(' a1 is smaller');
+let b1 = 80;
+b1 > 90 ? console.log('b1 is greater') : console.log('b1 is smaller');
+let c1 = 50;
+c1 > 40 ? console.log('c1 is greater') : console.log('c1 is smaller');
+
+let o1 = 600;
+let p1 = 770;
+console.log( o1 <= p1);
+
+let n = 70;
+let m = 80;
+console.log( n <= m);
+
+
+let y1 = 600;
+y1 > 500 ? console.log('y1 is greater ') : console.log('y1 is smaller')
+
+let text1 = 'A';
+let text2 = 65;
+let result = text1 === text2;
+console.log(result);
+
+let text3 ='Rohan' ;
+let text4 ='Rohan';
+let bharati = text3 == text4 ;
+console.log(bharati);
+
+let j = 70;
+let j1 = 60;
+console.log( j > j1 && j1 < j);
+
+let d1 = 200;
+let d2 = 400;
+
+console.log( d1 > d2 && d1 < d2); 
+
+console.log( d1 > d2 || d2 < d1);
+
+let s1 = true;
+console.log('!', !s1);
+
+
+
