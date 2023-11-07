4cac0c898b724274fbd8eb1bbcd6b6180149a93f
preity508 <preitypatil2022@gmail.com>
2023-11-07 19:08:19 +0530

    aaray methods

---
+++ javascript/arrayMethods.js
@@ -0,0 +1,91 @@
+let Ano = [ 10,20,30,40,50,60,70,80,90,100]
+console.log(Ano.length);
+
+let a = Ano.toString();
+console.log(a);
+
+let a2 = ['banana','mango','apple','orange'];
+let a3 = a2.pop(2);
+console.log(a2);
+console.log(a3);
+
+let x = [ 'mango','melon','lemon','kiwi']
+let x1 = x.pop()
+console.log(x);
+console.log(x1);
+
+let q = ['audi','seltos','BMW','mercedez','huyndai','city100']
+let q1 = q.pop();
+console.log(q);
+console.log(q1);
+
+let q2 = ['banana','mango','apple','orange'];
+let q3 = q2.push('kiwi')
+console.log(q2);
+console.log(q3);
+
+let a4 = ['audi','seltos','BMW','mercedez','huyndai','city100'];
+let a5 = a4.push('kiwi');
+console.log(a4);
+console.log(a5);
+ 
+let d = ['audi','seltos','BMW','mercedez','huyndai','city100'];
+let d1 = d.shift();
+console.log(d);
+console.log(d1);
+
+let y =  ['banana','mango','apple','orange'];
+let y1 = y.pop();
+console.log(y);
+console.log(y1);
+ 
+let y2 = y.push('kiwi');
+console.log(y);
+console.log('kiwi');
+
+let y3 = ['audi','seltos','BMW','mercedez','huyndai','city100'];
+let y4 = y3. push('KIA');
+console.log(y3);
+console.log(y4 );
+
+let t2 = ['audi','seltos','BMW','mercedez','huyndai','city100']
+let t3 = t2.shift();
+console.log(t2);
+console.log(t3);
+
+let fruits = ['banana','mango','apple','orange'];
+let fruits1 = fruits.unshift('KIWI','pineapple');
+console.log(fruits);
+console.log(fruits1);
+ 
+let fruits2 = ['banana','mango','apple','orange'];
+let fruits3 = ['kiwi','pineapple']
+let fruits4 = fruits2.concat(fruits3)
+console.log(fruits4);
+
+let r = ['banana','mango','apple','orange'];
+console.log(r.join('$'));
+
+let r1 = [ 8,8,2000];
+console.log(r1.join('/'));
+console.log(r1.join(' / '));
+console.log(r1.join('-'));
+console.log(r1.join(' - '));
+
+let r2 = [10,20,30,40,50,60,80,90]
+console.log(r2.slice(4));
+
+let fruits0 = ['banana','orange','apple','mango'];
+fruits0.splice(1 , 0 , ' bharati')
+console.log(fruits0);
+
+let fruit0 = ['banana','orange','apple','mango'];
+fruit0.splice(1,0,'kiwi')
+console.log(fruit0);
+
+fruit0.sort();
+console.log(fruit0);
+fruit0.reverse();
+
+
+let d2 = [ 20 , 30 ,60 , 80 ,10 ,40, 100];
---
+++ javascript/number.js
@@ -0,0 +1,28 @@
+
+//number methods//
+
+let a = 5000909;
+let a1 = a.toString();
+console.log(a1);
+
+let a3 = parseInt(a1);
+console.log(a1);
+
+let a4 = 50.6986;
+let a5 = a4.toFixed(4);
+console.log(a5);
+ 
+let wt = 50.3696;
+let b = wt.toFixed(3);
+console.log(b);
+
+let = b1 = 'bharati';
+b2 = b1.valueOf();
+console.log(b2);
+
+let v =  3.5678;
+let v1 = v.toFixed(3)
+console.log(v1);
+let c = 4.5678;
+let c1 = c.toFixed(2);
+console.log(c1);
